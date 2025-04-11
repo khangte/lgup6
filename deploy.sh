5e9e9cba75caddfc0b3f18f430f2b0b7b0ca8c90
khangte <minhyuk00321@gmail.com>
2025-04-11 09:53:04 +0900

    ${commit_message} - ${timestamp}

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ read -p "커밋 메시지를 입력하세요: " commit_message
 timestamp = $(date "+%Y-%m-%d %H:%M:%S")
 
 git add .
-git commit -m '$commit_message - $timestamp'
+git commit -m '${commit_message} - ${timestamp}'
 git push
