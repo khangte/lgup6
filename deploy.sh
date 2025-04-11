7dbbec801318dde2f49c66e8e636df6ba25e82fa
khangte <minhyuk00321@gmail.com>
2025-04-11 12:04:48 +0900

    updated | 2025-04-11 03:04:47 (KST)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-read -p "커밋 메시지를 입력하세요: " commit_message
+read -p "커밋 메시지를 입력하세요: " user_message
 
-current_time=$(TZ = Asia/Seoul date "+%Y-%m-%d %H:%M:%S")
+current_time=$(TZ=Asia/Seoul date "+%Y-%m-%d %H:%M:%S")
+
+commit_message="$user_message | $current_time (KST)"
 
 git add .
-git commit -m "$current_time $commit_message"
+git commit -m "$commit_message"
 git push
