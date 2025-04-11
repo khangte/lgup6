a926851578b66b98318d19b3156f7028b0bad05d
khangte <minhyuk00321@gmail.com>
2025-04-11 10:28:59 +0900

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 read -p "커밋 메시지를 입력하세요: " commit_message
 
-timestamp = $(date "+%Y-%m-%d %H:%M:%S")
+current_time=$(TZ = Asia/Seoul date "+%Y-%m-%d %H:%M:%S")
 
 git add .
-git commit -m "${commit_message} - ${timestamp}"
+git commit -m "$current_time $commit_message"
 git push
